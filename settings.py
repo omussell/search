bf0624f90037c45a66f13bceedc5a95169bb81d8
omussell <omussell@crossref.org>
2023-08-08 14:13:35 +0100

    fix(settings): Dont use dotenv and load the env vars correctly

--- settings.py
+++ settings.py
@@ -1,14 +1,11 @@
 import os
-import dotenv
-
-dotenv.load_dotenv(os.path.join(os.path.dirname(__file__), "config", ".env"))
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 SESSION_LIFETIME = 3600*24*30
 
-ORCID_CLIENT_ID = os.environ['ORCID_CLIENT_ID']
-ORCID_CLIENT_SECRET = os.environ['ORCID_CLIENT_SECRET']
+ORCID_CLIENT_ID = os.environ.get('ORCID_CLIENT_ID', "invalid")
+ORCID_CLIENT_SECRET = os.environ.get('ORCID_CLIENT_SECRET', "invalid")
 
 ORCID_SITE = os.environ.get('ORCID_SITE', "https://api.orcid.org")
 ORCID_AUTHORIZE_URL = os.environ.get("ORCID_AUTHORIZE_URL", "https://orcid.org/oauth/authorize")
