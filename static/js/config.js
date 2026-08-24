f4e27ac0fd0de354debde62fcc772bd51663c334
Oliver Mussell <oliver.mussell@hotmail.co.uk>
2026-08-24 21:10:43 +0100

    Refactor and simplify static assets (qwen3.8:27b)

--- static/js/config.js
+++
@@ -1,9 +0,0 @@
-MathJax.Hub.Config({
-  tex2jax: {
-    inlineMath: [ ['$','$$'], ["\\(","\\)"] ],
-    displayMath: [ ['$','$$'], ["\\[","\\]"] ],
-    processEscapes: true,
-    processEnvironments: true,
-    processClass: 'lead'
-  }
-});
